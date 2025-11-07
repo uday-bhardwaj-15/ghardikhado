5f56da619b90f2a45f3d3a70a134f6087b71d428
Uday Bhardwaj <142799565+uday-bhardwaj-15@users.noreply.github.com>
2025-11-07 13:18:34 +0530

    Final commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,4 @@
 import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import Hero from "@/components/Hero";
-import SearchBar from "@/components/SearchBar";
-import FeaturedProperties from "@/components/FeaturedProperties";
-import LatestListings from "@/components/LatestListings";
 
 export default function Home() {
   return (
